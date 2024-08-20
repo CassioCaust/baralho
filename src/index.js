893135dcb72c7c04d73815733ca53d5745fbc12e
CassioCaust <cassio.caust@gmail.com>
2024-08-19 21:41:36 -0300

    feat: Crud naipes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
+const naipe_controller = require("./controllers/naipe.js")
 const express = require('express');
 const app = express();
 const port = 3000;
 
+// Para próxima aula será melhor explicado (Midware)
+app.use(express.json());
+
 app.get("/naipe", (req, res) => {
-    res.send("get naipe 2")
+    const content = naipe_controller.index();
+    res.json(content)
+})
+
+app.get("/naipe/:id", (req, res) => {
+    const content = naipe_controller.show(req.params.id);
+    res.json(content)
+})
+
+app.post("/naipe",  (req, res) => {
+    const code = naipe_controller.store(req.body)
+    res.status(code).json()
+})
+
+app.put("/naipe/:id", (req, res) => {
+    const code = naipe_controller.update(req.params.id, req.body)
+    res.status(code).json()
+})
+
+app.delete("/naipe/:id", (req,res) => {
+    const code = naipe_controller.destroy(req.params.id)
+    res.status(code).json()
 })
 
 app.listen(port, () => {
